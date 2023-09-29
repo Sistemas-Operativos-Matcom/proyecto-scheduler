70eb711d1118844bc78e8b8b4a8df8ee9a3f13e8
AleTheCreation <alejandrito110402@gmail.com>
2023-09-29 14:17:10 -0400

    1.0.0

--- src/scheduler.c
+++ src/scheduler.c
@@ -33,6 +33,81 @@ int fifo_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
   return procs_info[0].pid;
 }
 
+int sjf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
+                     int curr_pid) {
+  // Ordenar los procesos por su tiempo de duración
+  for (int i = 0; i < procs_count - 1; i++) {
+    for (int j = i + 1; j < procs_count; j++) {
+      if (process_total_time(procs_info[i].pid) > process_total_time(procs_info[j].pid)) {
+        proc_info_t temp = procs_info[i];
+        procs_info[i] = procs_info[j];
+        procs_info[j] = temp;
+      }
+    }
+  }
+
+  // Retornar el PID del proceso con el menor tiempo de duración
+  return procs_info[0].pid;
+}
+int stcf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
+                     int curr_pid) {
+  // Ordenar los procesos por su tiempo de ejecución restante
+  for (int i = 0; i < procs_count - 1; i++) {
+    for (int j = i + 1; j < procs_count; j++) {
+      if (process_total_time(procs_info[i].pid) - procs_info[i].executed_time > process_total_time(procs_info[j].pid) - procs_info[j].executed_time) {
+        proc_info_t temp = procs_info[i];
+        procs_info[i] = procs_info[j];
+        procs_info[j] = temp;
+      }
+    }
+  }
+
+  // Retornar el PID del proceso con el menor tiempo de ejecución restante
+  return procs_info[0].pid;
+}
+
+int round_robin_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
+                     int curr_pid) {
+  // Calcular el tiempo de ejecución total de todos los procesos
+  int total_time = 0;
+  for (int i = 0; i < procs_count; i++) {
+    total_time += process_total_time(procs_info[i].pid);
+  }
+
+  // Calcular el time slice como el tiempo de ejecución total dividido por el número de procesos
+  int time_slice = total_time / procs_count;
+
+  // Mantener un índice que apunte al proceso actual
+  static int current_index = 0;
+
+  // Obtener el proceso actual
+  proc_info_t current_proc = procs_info[current_index];
+
+  // Verificar si el proceso actual ha terminado
+  if (process_total_time(current_proc.pid) - current_proc.executed_time  == 0) {
+    // Actualizar el índice al siguiente proceso
+    current_index = (current_index + 1) % procs_count;
+    current_proc = procs_info[current_index];
+  }
+
+  // Verificar si el proceso actual ha excedido el time slice
+  if (current_proc.executed_time % time_slice == 0 && current_proc.executed_time != 0) {
+    // Actualizar el índice al siguiente proceso
+    current_index = (current_index + 1) % procs_count;
+    current_proc = procs_info[current_index];
+  }
+
+  // Actualizar el tiempo ejecutado del proceso actual
+  current_proc.executed_time++;
+
+  // Actualizar el tiempo restante de ejecución del proceso actual
+  //current_proc.executed_time++;
+
+  // Retornar el PID del proceso actual
+  return current_proc.pid;
+}
+
+
 int my_own_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
                      int curr_pid) {
   // Implementa tu scheduler aqui ... (el nombre de la función lo puedes
