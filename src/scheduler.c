6d4a6a7c8710cfa989eb54414181ab9522621766
josem-nex <josem.zayas@estudiantes.matcom.uh.cu>
2023-10-04 03:42:50 -0400

    queue and rr

--- src/scheduler.c
+++ src/scheduler.c
@@ -6,9 +6,9 @@
 #include <time.h>
 #include "queue.h"
 #include "simulation.h"
-#define QUANTUM 20
+#define QUANTUM 50
 
-Queue ready_queue;
+// Queue ready_queue;
 
 // La función que define un scheduler está compuesta por los siguientes
 // parámetros:
@@ -47,8 +47,7 @@ int fifo_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
 
   return procs_info[0].pid;
 }
-int my_SJF(proc_info_t *procs_info, int procs_count, int curr_time,
-           int curr_pid)
+int my_SJF(proc_info_t *procs_info, int procs_count, int curr_time, int curr_pid)
 {
   if (curr_pid == -1)
   {
@@ -70,8 +69,7 @@ int my_SJF(proc_info_t *procs_info, int procs_count, int curr_time,
     return curr_pid;
   }
 }
-int my_STCF(proc_info_t *procs_info, int procs_count, int curr_time,
-            int curr_pid)
+int my_STCF(proc_info_t *procs_info, int procs_count, int curr_time, int curr_pid)
 {
   proc_info_t procmin = procs_info[0];
   int TTmin = process_total_time(procmin.pid);
@@ -86,88 +84,15 @@ int my_STCF(proc_info_t *procs_info, int procs_count, int curr_time,
   }
   return procmin.pid;
 }
-int my_RR2(proc_info_t *procs_info, int procs_count, int curr_time,
-           int curr_pid)
+int my_RR(proc_info_t *procs_info, int procs_count, int curr_time, int curr_pid)
 {
-  if (curr_pid != -1)
-  {
-    int pos = findPID(procs_info, procs_count, curr_pid);
-    if (procs_info[pos].executed_time % QUANTUM != 0)
-      return curr_pid;
-    else
-    {
-      actQueue(procs_info, procs_count, curr_pid);
-      enqueue(&ready_queue, procs_info[pos]);
-      proc_info_t next_proc = dequeue(&ready_queue);
-      return next_proc.pid;
-    }
-  }
-  else
-  {
-    actQueue(procs_info, procs_count, curr_pid);
-    proc_info_t next_proc = dequeue(&ready_queue);
-    return next_proc.pid;
-  }
+  static int pos = -1;
+  if (curr_time % QUANTUM == 0 || curr_pid == -1)
+    return procs_info[++pos % procs_count].pid;
+  return curr_pid;
 }
-void actQueue(proc_info_t *procs_info, int procs_count,
-              int curr_pid)
-{
-  for (int i = 0; i < procs_count; i++)
-  {
-    /* if (procs_info[i].pid == curr_pid)
-    {
-      continue;
-    } */
 
-    if (contains(&ready_queue, procs_info[i]) == 0)
-    {
-      enqueue(&ready_queue, procs_info[i]);
-    }
-  }
-}
-int my_RR(proc_info_t *procs_info, int procs_count, int curr_time,
-          int curr_pid)
-{
-  /* annado a la cola los procesos nuevos que llegaron */
-  int pos = -1;
-  for (int i = 0; i < procs_count; i++)
-  {
-    if (procs_info[i].pid == curr_pid)
-    {
-      pos = i;
-      continue;
-    }
-
-    if (contains(&ready_queue, procs_info[i]) == 0)
-    {
-      enqueue(&ready_queue, procs_info[i]);
-    }
-  }
-  /* busco cual es el proceso del pid actual y verifico si ya cumplio su quantum */
-  // int pos = findPID(procs_info, procs_count, curr_pid);
-  if (curr_pid != -1 && pos != -1)
-  {
-    printf("si");
-    if (procs_info[pos].executed_time % QUANTUM == 0)
-    {
-      printf("\nok:%i", curr_pid);
-      enqueue(&ready_queue, procs_info[pos]);
-    }
-    else
-    {
-      return curr_pid;
-    }
-  }
-  /* si la cola no esta vacia hago pop y retorno ese pid */
-  if (!isEmpty(&ready_queue))
-  {
-    proc_info_t next_proc = dequeue(&ready_queue);
-    printf("perfe: %i  cant cola:%i\n", next_proc.pid, ready_queue.pop);
-    return next_proc.pid;
-  }
-  return -1;
-}
-int findPID(proc_info_t *procs_info, int procs_count, int pid)
+/* int findPID(proc_info_t *procs_info, int procs_count, int pid)
 {
   for (int i = 0; i < procs_count; i++)
   {
@@ -177,7 +102,7 @@ int findPID(proc_info_t *procs_info, int procs_count, int pid)
     }
   }
   return -1;
-}
+} */
 int my_own_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
                      int curr_pid)
 {
@@ -202,6 +127,7 @@ int my_own_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
 // según el nombre del scheduler.
 schedule_action_t get_scheduler(const char *name)
 {
+  // initQueue(&ready_queue);
 
   // Si necesitas inicializar alguna estructura antes de comenzar la simulación
   // puedes hacerlo aquí.
@@ -213,10 +139,7 @@ schedule_action_t get_scheduler(const char *name)
   if (strcmp(name, "stcf") == 0)
     return *my_STCF;
   if (strcmp(name, "rr") == 0)
-  {
-    initQueue(&ready_queue);
-    return *my_RR2;
-  }
+    return *my_RR;
 
   fprintf(stderr, "Invalid scheduler name: '%s'\n", name);
   exit(1);
