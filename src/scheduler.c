3ccaf5ac71e9a7e1a77bece12a1dad89f28d9bcb
JoseAConcepcion <concepcijoseantonio@gmail.com>
2023-10-05 22:49:57 -0400

    :white_check_mark: SJF implemented

--- src/scheduler.c
+++ src/scheduler.c
@@ -1,5 +1,5 @@
 #include "scheduler.h"
-
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,6 +28,7 @@
 //* - La función devuelve un PID diferente al curr_pid: Simula un cambio de
 //*     contexto y se ejecuta el proceso indicado.
 
+int latest_sj = 0;
 
 
 int fifo_scheduler(proc_info_t *procs_info, int procs_count, int curr_time, int curr_pid)
@@ -36,14 +37,19 @@ int fifo_scheduler(proc_info_t *procs_info, int procs_count, int curr_time, int 
 }
 
 int sjf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time, int curr_pid) 
-{
-  
-  int pid = procs_info[0].pid;
-  int on_io = procs_info[0].on_io;  
-  int exec_time = procs_info[0].executed_time;
-  int duration = process_total_time(pid);
-
-  return -1;
+{ 
+  if(curr_pid == latest_sj) return curr_pid;
+   
+  int shortest_job = INT_MAX;
+  for (size_t i = 0; i < procs_count; i++)
+  {
+    if (process_total_time(procs_info[i].pid) < shortest_job)
+    {
+      shortest_job = process_total_time(procs_info[i].pid);
+    }
+  }
+  latest_sj = shortest_job;
+  return shortest_job; 
 }
 
 int stcf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time, int curr_pid) 
