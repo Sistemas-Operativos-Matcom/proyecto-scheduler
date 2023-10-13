0e462b3c47a323c974cb4f0decf325f9da0c8d0b
JackRipper01 <francohernandezpiloto@gmail.com>
2023-10-12 22:43:05 -0400

    first_and_hopefully_last_commit

--- src/scheduler.c
+++ src/scheduler.c
@@ -7,6 +7,10 @@
 
 #include "simulation.h"
 
+int actual_time_interrupt_count_to_time_slice = 0;
+int round_robin_process_counter = 0;
+int Time_Slice = 50;
+
 // La función que define un scheduler está compuesta por los siguientes
 // parámetros:
 //
@@ -27,38 +31,126 @@
 //  contexto y se ejecuta el proceso indicado.
 //
 int fifo_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
-                   int curr_pid) {
+                   int curr_pid)
+{
   // Se devuelve el PID del primer proceso de todos los disponibles (los
   // procesos están ordenados por orden de llegada).
   return procs_info[0].pid;
 }
 
-int my_own_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
-                     int curr_pid) {
-  // Implementa tu scheduler aqui ... (el nombre de la función lo puedes
-  // cambiar)
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  // Información que puedes obtener de un proceso
-  int pid = procs_info[0].pid;      // PID del proceso
-  int on_io = procs_info[0].on_io;  // Indica si el proceso se encuentra
-                                    // realizando una opreación IO
-  int exec_time = procs_info[0].executed_time;  // Tiempo que el proceso se ha
-                                                // ejecutado (en CPU o en I/O)
+int SJF(proc_info_t *procs_info, int procs_count, int curr_time,
+        int curr_pid)
+{
+
+  if (curr_pid != -1)
+  {
+    return curr_pid;
+  }
+
+  int min_total_time = process_total_time(procs_info[0].pid);
+  int next_procs_pid = procs_info[0].pid;
+  for (int i = 0; i < procs_count; i++)
+  {
+    if (min_total_time > process_total_time(procs_info[i].pid))
+    {
+      next_procs_pid = procs_info[i].pid;
+      min_total_time = process_total_time(procs_info[i].pid);
+    }
+  }
+
+  return next_procs_pid;
+
+  /*// Información que puedes obtener de un proceso
+  int pid = procs_info[0].pid;                 // PID del proceso
+  int on_io = procs_info[0].on_io;             // Indica si el proceso se encuentra
+                                               // realizando una opreación IO
+  int exec_time = procs_info[0].executed_time; // Tiempo que el proceso se ha
+                                               // ejecutado (en CPU o en I/O)
 
   // También puedes usar funciones definidas en `simulation.h` para extraer
   // información extra:
-  int duration = process_total_time(pid);
-
-  return -1;
+  int duration = process_total_time(pid);*/
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int STCF(proc_info_t *procs_info, int procs_count, int curr_time,
+         int curr_pid)
+{
+
+  int min_duration_time_to_finish = process_total_time(procs_info[0].pid) - procs_info[0].executed_time;
+  int next_procs_pid = procs_info[0].pid;
+  for (int i = 0; i < procs_count; i++)
+  {
+    int process_time_to_finish = process_total_time(procs_info[i].pid) - procs_info[i].executed_time;
+    if (min_duration_time_to_finish > process_time_to_finish)
+    {
+      next_procs_pid = procs_info[i].pid;
+      min_duration_time_to_finish = process_time_to_finish;
+    }
+  }
+
+  return next_procs_pid;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int Round_Robin(proc_info_t *procs_info, int procs_count, int curr_time,
+                int curr_pid)
+{
+
+  // actual_time_interrupt_count_to_time_slice: es la cantidad d time interrupt que un proceso
+  // tiene permitido seguir ejecutandose(saltarse)
+
+  if (actual_time_interrupt_count_to_time_slice == Time_Slice / 10) // : si ya cumpliste con tu slice time...
+  {
+    actual_time_interrupt_count_to_time_slice = 0;
+
+    // round_robin_process_counter: es el index del proceso que se esta ejecutando en el array procs_info
+
+    if (round_robin_process_counter < procs_count - 1) // : si no eres el ultimo...
+    {
+      round_robin_process_counter++;
+      return procs_info[round_robin_process_counter].pid; // proceso q se va a ejecutar
+    }
+    else
+    {
+      round_robin_process_counter = 0;
+      return procs_info[0].pid;
+    }
+  }
+
+  if (curr_pid != -1) //= si todavia no has cumplido con tu slice time y no has terminado de ejecutarte...
+  {
+    actual_time_interrupt_count_to_time_slice++;
+    return curr_pid;
+  }
+  else
+  {
+    actual_time_interrupt_count_to_time_slice = 0;
+    return procs_info[round_robin_process_counter].pid;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////
 
 // Esta función devuelve la función que se ejecutará en cada timer-interrupt
 // según el nombre del scheduler.
-schedule_action_t get_scheduler(const char *name) {
+schedule_action_t get_scheduler(const char *name)
+{
   // Si necesitas inicializar alguna estructura antes de comenzar la simulación
   // puedes hacerlo aquí.
 
-  if (strcmp(name, "fifo") == 0) return *fifo_scheduler;
+  if (strcmp(name, "fifo") == 0)
+    return *fifo_scheduler;
+  if (strcmp(name, "SJF") == 0)
+    return *SJF;
+  if (strcmp(name, "STCF") == 0)
+    return *STCF;
+  if (strcmp(name, "Round_Robin") == 0)
+    return *Round_Robin;
 
   // Añade aquí los schedulers que implementes. Por ejemplo:
   //
