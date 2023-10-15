f6360539c5a6a35a3fa37a1ec4da30d5005970ed
¨NinaSayers¨ <massypaz2001git@gmail.com>
2023-10-14 21:59:12 -0400

    sjf, stcf, rr

--- src/scheduler.c
+++ src/scheduler.c
@@ -38,8 +38,13 @@ int sjf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time, int c
 {
   int index = 0;
   int best_time = process_total_time(procs_info[index].pid);
-  for (int i = 1; i < procs_count; i++)
+  for (int i = 0; i < procs_count; i++)
   {
+    //ejecuta 1 proceso hasta el final
+    if(procs_info[i].pid == curr_pid && procs_info[i].executed_time > 0) return curr_pid; 
+    
+    //de los procesos pendientes (ninguno ha comenzado su ejecucion) selecciona 
+    //el de menor tiempo de ejecucion
     if(process_total_time(procs_info[i].pid) < best_time)
     {
       best_time = process_total_time(procs_info[i].pid);
@@ -48,6 +53,49 @@ int sjf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time, int c
   } 
   return procs_info[index].pid; 
 }
+
+
+int stcf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time, int curr_pid)
+{
+  int index = 0;
+  int best_time = process_total_time(procs_info[index].pid) - procs_info[index].executed_time;
+
+  for (int i = 1; i < procs_count; i++)
+  {
+    //de los procesos activos, selecciona el que le quede menor tiempo de ejecucion
+     if(process_total_time(procs_info[i].pid) - procs_info[i].executed_time < best_time)
+    {
+      best_time = process_total_time(procs_info[i].pid) - procs_info[i].executed_time;
+      index = i;
+    }
+  }
+  return procs_info[index].pid;
+}
+
+
+int rr_scheduler(proc_info_t *procs_info, int procs_count, int curr_time, int curr_pid)
+{
+  if (procs_count == 1) return procs_info[0].pid;
+
+  for (int i = 0; i < procs_count; i++)
+  {
+    if (procs_info[i].pid != curr_pid) continue;
+
+    //slice_time = 40 
+    if(curr_time % 40 != 0) return procs_info[i].pid;
+
+    //si el proceso que actualmente ejecuta el CPU NO es el
+    //ultimo en la lista de procesos activos, lo interrumpe y 
+    //ejecuta el siguiente
+    if(i < procs_count - 1) return procs_info[i + 1].pid;
+    else break;    
+  } 
+  return procs_info[0].pid;
+}  
+
+
+
+
 
 
 
@@ -78,6 +126,8 @@ schedule_action_t get_scheduler(const char *name) {
 
   if (strcmp(name, "fifo") == 0) return *fifo_scheduler;
   if (strcmp(name, "sjf") == 0) return *sjf_scheduler;
+  if (strcmp(name, "stcf") == 0) return *stcf_scheduler;
+  if (strcmp(name, "rr") == 0) return *rr_scheduler;
 
   // Añade aquí los schedulers que implementes. Por ejemplo:
   //
