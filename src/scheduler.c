5e061e161ad6cf1a9671932ccfd865304265a92e
Rafael Acosta <theGitNoob@gmail.com>
2023-10-15 15:07:04 -0400

    Implemented RR and STCF

--- src/scheduler.c
+++ src/scheduler.c
@@ -27,23 +27,25 @@
 //  contexto y se ejecuta el proceso indicado.
 //
 int fifo_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
-                   int curr_pid) {
+                   int curr_pid)
+{
   // Se devuelve el PID del primer proceso de todos los disponibles (los
   // procesos están ordenados por orden de llegada).
   return procs_info[0].pid;
 }
 
-int my_own_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
-                     int curr_pid) {
+int mlfq_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
+                   int curr_pid)
+{
   // Implementa tu scheduler aqui ... (el nombre de la función lo puedes
   // cambiar)
 
   // Información que puedes obtener de un proceso
-  int pid = procs_info[0].pid;      // PID del proceso
-  int on_io = procs_info[0].on_io;  // Indica si el proceso se encuentra
-                                    // realizando una opreación IO
-  int exec_time = procs_info[0].executed_time;  // Tiempo que el proceso se ha
-                                                // ejecutado (en CPU o en I/O)
+  int pid = procs_info[0].pid;                 // PID del proceso
+  int on_io = procs_info[0].on_io;             // Indica si el proceso se encuentra
+                                               // realizando una opreación IO
+  int exec_time = procs_info[0].executed_time; // Tiempo que el proceso se ha
+                                               // ejecutado (en CPU o en I/O)
 
   // También puedes usar funciones definidas en `simulation.h` para extraer
   // información extra:
@@ -51,19 +53,77 @@ int my_own_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
 
   return -1;
 }
+int rr_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
+                 int curr_pid)
+{
+  // int time_slice = 1 * 10;
+  if (curr_pid == -1)
+    return procs_info[0].pid;
 
+  int active_pr_count = 0;
+
+  for (int idx = 0; idx < procs_count; idx++)
+  {
+    if (procs_info[idx].executed_time <= process_total_time(procs_info[idx].pid))
+      active_pr_count += 1;
+  }
+
+  int *active_process = (int *)malloc(active_pr_count * sizeof(int));
+  int curr = 0;
+
+  for (int idx = 0; idx < procs_count; idx++)
+    if (procs_info[idx].executed_time <= process_total_time(procs_info[idx].pid))
+      active_process[curr++] = procs_info[idx].pid;
+
+  for (int idx = 0; idx < active_pr_count; idx++)
+    if (active_process[idx] == curr_pid)
+      return active_process[(idx + 1) % procs_count];
+
+  // return active_process[0];
+  // fprintf(stderr, "RR scheduler Not Implemented ");
+}
+int stcf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
+                   int curr_pid)
+{
+  int stcf_pid = -1;
+  int stcf_time = __INT_MAX__;
+  for (int idx = 0; idx < procs_count; idx++)
+  {
+    int time_left = process_total_time(procs_info[idx].pid) - procs_info[idx].executed_time;
+    if (time_left >= 0 && time_left < stcf_time)
+    {
+      stcf_time = time_left;
+      stcf_pid = procs_info[idx].pid;
+    }
+  }
+  return stcf_pid;
+}
+int sjf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
+                  int curr_pid)
+{
+  fprintf(stderr, "SJF scheduler Not Implemented ");
+  exit(1);
+}
 // Esta función devuelve la función que se ejecutará en cada timer-interrupt
 // según el nombre del scheduler.
-schedule_action_t get_scheduler(const char *name) {
+schedule_action_t get_scheduler(const char *name)
+{
   // Si necesitas inicializar alguna estructura antes de comenzar la simulación
   // puedes hacerlo aquí.
 
-  if (strcmp(name, "fifo") == 0) return *fifo_scheduler;
+  if (strcmp(name, "fifo") == 0)
+    return *fifo_scheduler;
 
   // Añade aquí los schedulers que implementes. Por ejemplo:
-  //
-  // if (strcmp(name, "sjf") == 0) return *sjf_scheduler;
-  //
+
+  if (strcmp(name, "sjf") == 0)
+    return *sjf_scheduler;
+  if (strcmp(name, "stcf") == 0)
+    return *stcf_scheduler;
+  if (strcmp(name, "rr") == 0)
+    return *rr_scheduler;
+  if (strcmp(name, "mlfq") == 0)
+    return *mlfq_scheduler;
 
   fprintf(stderr, "Invalid scheduler name: '%s'\n", name);
   exit(1);
