1ae32056ba7c3150aaaf493e4f6b6fa1dd6257f5
adrianfoya <adriannf0108@gmail.com>
2023-10-15 17:30:51 -0400

    stcf fixed added comments

--- src/scheduler.c
+++ src/scheduler.c
@@ -24,7 +24,8 @@
 //  proceso actual.
 //  - La función devuelve un PID diferente al curr_pid: Simula un cambio de
 //  contexto y se ejecuta el proceso indicado.
-//
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 int fifo_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
                    int curr_pid)
 {
@@ -33,25 +34,7 @@ int fifo_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
   return procs_info[0].pid;
 }
 
-/*int my_own_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
-                     int curr_pid)
-{
-  // Implementa tu scheduler aqui ... (el nombre de la función lo puedes
-  // cambiar)
-
-  // Información que puedes obtener de un proceso
-  int pid = procs_info[0].pid;                 // PID del proceso
-  int on_io = procs_info[0].on_io;             // Indica si el proceso se encuentra
-                                               // realizando una opreación IO
-  int exec_time = procs_info[0].executed_time; // Tiempo que el proceso se ha
-                                               // ejecutado (en CPU o en I/O)
-
-  // También puedes usar funciones definidas en `simulation.h` para extraer
-  // información extra:
-  int duration = process_total_time(pid);
-
-  return -1;
-}*/
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 int sjf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
                   int curr_pid)
@@ -76,47 +59,35 @@ int sjf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
   return result;
 }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 int stcf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
                    int curr_pid)
 {
-  int min;    // tiempo de ejecucion del proceso
+  int min;    // tiempo de ejecucion que le queda al proceso
   int result; // el pid del proceso
-  min = process_total_time(procs_info[0].pid);
+  min = process_total_time(procs_info[0].pid) - procs_info[0].executed_time;
   result = procs_info[0].pid;
 
-  for (size_t j = 0; j < procs_count; j++)
+  for (size_t j = 0; j < procs_count; j++) //iteramos buscando el proceso que menos tiempo de ejecucuion le queda
   {
-    if (process_total_time(procs_info[j].pid) < min)
+    if (process_total_time(procs_info[j].pid) - procs_info[j].executed_time < min)
     {
-      min = process_total_time(procs_info[j].pid);
+      min = process_total_time(procs_info[j].pid) - procs_info[j].executed_time;
       result = procs_info[j].pid;
     }
   }
 
-  if (curr_pid == -1)
-    return result;
-
-  int actual;
-  for (size_t i = 0; i < procs_count; i++)
-  {
-    if (procs_info[i].pid == curr_pid)
-    {
-      actual = process_total_time(curr_pid) - procs_info[i].executed_time;
-      break;
-    }
-  }
-
-  if (min < actual)
-    return result;
-  return curr_pid;
-  // si hay un proceso ejecutandose entonces se termina de ejecutar
-  //  de lo contario buscamos el proceso con menor tiempo de ejecucion y retornamos su pid
+  return result;
 }
-int position = 0;
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int position = 0; //variable global en al que guardamos la posicion del proximo proceso a ejecutar
 int rr_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
                  int curr_pid)
 {
-  if (position >= procs_count)
+  if (position >= procs_count) //si la posicion del proceso a ejecutar esta fuera del tamanno del array ejecuto el primer proceso
   {
     position = 1;
     return procs_info[0].pid;
@@ -124,33 +95,7 @@ int rr_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
   position = position + 1;
   return procs_info[position - 1].pid;
 }
-
-/*int rr_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
-                  int curr_pid)
-{
-  for (size_t i = 0; i < procs_count; i++)
-  {
-    if(procs_info[i].pid == curr_pid)
-    {
-      if(i == procs_count-1) return procs_info[0].pid;
-      return procs_info[i+1].pid;
-    }
-  }
-  return procs_info[0].pid;
-}*/
-
-/*int rr_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
-                  int curr_pid)
-{
-  proc_info_t process = procs_info[0];
-  for (size_t i = 1; i < procs_count; i++)
-  {
-    procs_info[i-1] = procs_info[i];
-  }
-  procs_info[procs_count-1] = process;
-  return process.pid;
-
-}*/
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // Esta función devuelve la función que se ejecutará en cada timer-interrupt
 // según el nombre del scheduler.
