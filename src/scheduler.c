d4ec157b12ab797249a9c5fa2aabed0db81dd168
Bruno Pire <br1.pire@gamil.com>
2023-10-15 20:11:04 -0400

    all

--- src/scheduler.c
+++ src/scheduler.c
@@ -7,7 +7,13 @@
 
 #include "simulation.h"
 
-int *act_proc=-1;
+int act_proc=-1;
+
+int *pid_save;
+
+int pid_save_size = 0;
+
+int s=5;
 
 
 int check_PID(proc_info_t *procs_info, int count, int act_proc_PID){
@@ -22,6 +28,26 @@ for (size_t i = 0; i < count; i++)
 return 0;
 
 }
+
+void sort(proc_info_t *procs_info,int procs_count){
+
+for (size_t i = 0; i < procs_count; i++)
+       {
+        for (size_t j = i+1; i < procs_count; j++)
+        {
+          if( process_total_time(procs_info[i].pid)> process_total_time(procs_info[j].pid)){
+            proc_info_t swap = procs_info[i];
+            procs_info[i]=procs_info[j];
+            procs_info[i]=swap;
+          }
+        }
+        
+       }
+
+
+}
+
+
 // La función que define un scheduler está compuesta por los siguientes
 // parámetros:
 //
@@ -41,6 +67,8 @@ return 0;
 //  - La función devuelve un PID diferente al curr_pid: Simula un cambio de
 //  contexto y se ejecuta el proceso indicado.
 //
+
+
 int fifo_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
                    int curr_pid) {
   // Se devuelve el PID del primer proceso de todos los disponibles (los
@@ -68,61 +96,110 @@ int my_own_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
   return -1;
 }
 
-int sjf(proc_info_t *procs_info, int procs_count, int curr_time,int curr_pid){
+int sjf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,int curr_pid){
 
 if (check_PID(procs_info,procs_count,act_proc))
 {
   return act_proc;
 }
 
-proc_info_t *procs_info_copy =procs_info;
+// proc_info_t *procs_info_copy =procs_info;
 
-       for (size_t i = 0; i < procs_count; i++)
-       {
-        for (size_t j = i+1; i < procs_count; j++)
-        {
-          if( process_total_time(procs_info_copy[i].pid)> process_total_time(procs_info_copy[j].pid)){
-            proc_info_t swap = procs_info_copy[i];
-            procs_info_copy[i]=procs_info_copy[j];
-            procs_info_copy[i]=swap;
-          }
-        }
+//        for (size_t i = 0; i < procs_count; i++)
+//        {
+//         for (size_t j = i+1; i < procs_count; j++)
+//         {
+//           if( process_total_time(procs_info_copy[i].pid)> process_total_time(procs_info_copy[j].pid)){
+//             proc_info_t swap = procs_info_copy[i];
+//             procs_info_copy[i]=procs_info_copy[j];
+//             procs_info_copy[i]=swap;
+//           }
+//         }
         
-       }
+//        }
 
-       act_proc=procs_info_copy[0].pid;
-       return act_proc;
+sort(procs_info,procs_count);
+
+act_proc=procs_info[0].pid;
+return act_proc;
 
 
 }
 
 
-int stcf(proc_info_t *procs_info, int procs_count, int curr_time,int curr_pid){
+int stcf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,int curr_pid){
 
-       proc_info_t *procs_info_copy =procs_info;
+      //  proc_info_t *procs_info_copy =procs_info;
 
-       for (size_t i = 0; i < procs_count; i++)
-       {
-        for (size_t j = i+1; i < procs_count; j++)
-        {
-          if( process_total_time(procs_info_copy[i].pid)> process_total_time(procs_info_copy[j].pid)){
-            proc_info_t swap = procs_info_copy[i];
-            procs_info_copy[i]=procs_info_copy[j];
-            procs_info_copy[i]=swap;
-          }
-        }
+      //  for (size_t i = 0; i < procs_count; i++)
+      //  {
+      //   for (size_t j = i+1; i < procs_count; j++)
+      //   {
+      //     if( process_total_time(procs_info_copy[i].pid)> process_total_time(procs_info_copy[j].pid)){
+      //       proc_info_t swap = procs_info_copy[i];
+      //       procs_info_copy[i]=procs_info_copy[j];
+      //       procs_info_copy[i]=swap;
+      //     }
+      //   }
         
-       }
+      //  }
 
-       return procs_info_copy[0].pid;
+sort(procs_info,procs_count);
+
+       return procs_info[0].pid;
            
 }
 
-int rr(proc_info_t *procs_info, int procs_count, int curr_time,int curr_pid){
+int rr_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,int curr_pid){
 
 
 
+int* aux=malloc(pid_save_size * sizeof(int));
+
+for (size_t i = 0; i < pid_save_size; i++)
+{
+  aux[i]=pid_save[i];
 }
+
+pid_save= malloc(procs_count * sizeof(int));
+int aux_size= pid_save_size;
+pid_save_size=procs_count;
+
+for (size_t i = 1; i < aux_size; i++)
+{
+  pid_save[i-1]=aux[i];
+}
+
+
+for (size_t i = 0; i < procs_count; i++)
+{
+  int is=0;
+
+  for (size_t j = 0; i < pid_save_size; i++)
+  {
+    if(procs_info[i].pid==pid_save[j]){
+
+      is=1;
+      break;
+
+    }
+  }
+
+  if (is==0){
+
+  pid_save[aux_size]=procs_info[i].pid;
+  aux_size++;
+
+  }
+
+  return pid_save[0];
+    
+}
+  
+}
+
+
+
 
 // Esta función devuelve la función que se ejecutará en cada timer-interrupt
 // según el nombre del scheduler.
@@ -131,6 +208,9 @@ schedule_action_t get_scheduler(const char *name) {
   // puedes hacerlo aquí.
 
   if (strcmp(name, "fifo") == 0) return *fifo_scheduler;
+  if (strcmp(name, "sjf") == 0) return *sjf_scheduler;
+  if (strcmp(name, "stcf") == 0) return *stcf_scheduler;
+  if (strcmp(name, "rr") == 0) return *rr_scheduler;
 
   // Añade aquí los schedulers que implementes. Por ejemplo:
   //
