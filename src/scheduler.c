89c2c54651182f6fdbabe163c5ae4dc59526610d
DanielMPMatCom <daniel.machado.0206@gmail.com>
2023-10-15 23:17:17 -0400

    mlfq broken

--- src/scheduler.c
+++ src/scheduler.c
@@ -6,7 +6,6 @@
 #include <time.h>
 
 #include "simulation.h"
-#include "queue.h" // Incluimos la definición de la cola
 
 // La función que define un scheduler está compuesta por los siguientes
 // parámetros:
@@ -35,25 +34,25 @@ int fifo_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
   return procs_info[0].pid;
 }
 
-int my_own_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
-                     int curr_pid)
-{
-  // Implementa tu scheduler aqui ... (el nombre de la función lo puedes
-  // cambiar)
+// int my_own_scheduler(proc_info_t *procs_info, int procs_count, int curr_time,
+//                      int curr_pid)
+// {
+//   // Implementa tu scheduler aqui ... (el nombre de la función lo puedes
+//   // cambiar)
 
-  // Información que puedes obtener de un proceso
-  int pid = procs_info[0].pid;                 // PID del proceso
-  int on_io = procs_info[0].on_io;             // Indica si el proceso se encuentra
-                                               // realizando una opreación IO
-  int exec_time = procs_info[0].executed_time; // Tiempo que el proceso se ha
-                                               // ejecutado (en CPU o en I/O)
+//   // Información que puedes obtener de un proceso
+//   int pid = procs_info[0].pid;                 // PID del proceso
+//   int on_io = procs_info[0].on_io;             // Indica si el proceso se encuentra
+//                                                // realizando una opreación IO
+//   int exec_time = procs_info[0].executed_time; // Tiempo que el proceso se ha
+//                                                // ejecutado (en CPU o en I/O)
 
-  // También puedes usar funciones definidas en `simulation.h` para extraer
-  // información extra:
-  int duration = process_total_time(pid);
+//   // También puedes usar funciones definidas en `simulation.h` para extraer
+//   // información extra:
+//   int duration = process_total_time(pid);
 
-  return -1;
-}
+//   return -1;
+// }
 int sjf_scheduler(proc_info_t *procs_info, int procs_count, int curr_time, int curr_pid)
 {
   if (curr_pid != -1)
@@ -108,9 +107,130 @@ int rr_scheduler(proc_info_t *procs_info, int procs_count, int curr_time, int cu
     return procs_info[proc_index = (proc_index % procs_count)].pid;
   }
 }
+int pids[MAX_PROCESS_COUNT];
+int priorities[MAX_PROCESS_COUNT];
+int timeInQueues[MAX_PROCESS_COUNT];
+int priorityBoostTime = 0;
+int lastProcessIndex = 0;
+int queuesCount = 3;
+
+void update(proc_info_t *procs_info, int procs_count)
+{
+  int pid_index = 0;
+  int flag = 1;
+  for (int i = 0; i < procs_count; i++)
+  {
+    for (int j = 0; j < lastProcessIndex; j++)
+    {
+      if (procs_info[i].pid == pids[j])
+      {
+        pids[pid_index] = pids[j];
+        priorities[pid_index] = priorities[j];
+        timeInQueues[pid_index] = timeInQueues[j] + 1;
+        pid_index++;
+        flag = 0;
+      }
+    }
+    if (flag)
+    {
+      for (int k = i; k < procs_count; k++)
+      {
+        pids[pid_index] = procs_info[k].pid;
+        priorities[pid_index] = 0;
+        timeInQueues[pid_index] = 0;
+        pid_index++;
+      }
+      break;
+    }
+  }
+  lastProcessIndex = procs_count - 1;
+}
+
+void priority_boost()
+{
+  for (int i = 0; i <= lastProcessIndex; i++)
+  {
+    priorities[i] = 0;
+    timeInQueues[i] = 0;
+  }
+}
+
+int FindNextProcess(proc_info_t *procs_info, int curr_time, int curr_pid)
+{
+  int countOfQueue = 0;
+  int currQueue = 0;
+  for (int k = 0; k < queuesCount; k++)
+  {
+    for (int i = 0; i < lastProcessIndex; i++)
+    {
+      if (priorities[i] == k && !procs_info[i].on_io)
+      {
+        countOfQueue++;
+      }
+    }
+    if (countOfQueue > 0)
+    {
+      currQueue = k;
+      break;
+    }
+  }
+  if (countOfQueue == 0)
+    return -1;
+  proc_info_t procsInQueue[countOfQueue];
+  int indexForRR = 0;
+  for (int t = 0; t < lastProcessIndex; t++)
+  {
+    if (priorities[t] == currQueue && !procs_info[t].on_io)
+    {
+      procsInQueue[indexForRR] = procs_info[t];
+      indexForRR++;
+    }
+  }
+  int pidForReturn = rr_scheduler(procsInQueue, countOfQueue, curr_time, curr_pid);
+  for (int i = 0; i < lastProcessIndex; i++)
+  {
+    if (pids[i] == pidForReturn)
+    {
+      timeInQueues[i]++;
+      if (timeInQueues[i] == 3 && priorities[i] < queuesCount - 1)
+      {
+        priorities[i]++;
+      }
+    }
+  }
+  return pidForReturn;
+}
 int mlfq_scheduler(proc_info_t *procs_info, int procs_count, int curr_time, int curr_pid)
 {
-  
+  for( int i = 0; i< procs_count; i++)
+  {
+    printf("%d", procs_info[i].pid);
+  }
+  // for( int i = 0; i< procs_count; i++)
+  // {
+  //   printf("%d", pids[i]);
+  // }
+  update(procs_info, procs_count);
+  if (priorityBoostTime == 6)
+  {
+    priority_boost();
+    priorityBoostTime = 0;
+  }
+  else
+  {
+    priorityBoostTime++;
+  }
+  int a = FindNextProcess(procs_info, curr_time, curr_pid);
+  // for( int i = 0; i< procs_count; i++)
+  // {
+  //   printf("%d", procs_info[i].pid);
+  // }
+  // for( int i = 0; i< procs_count; i++)
+  // {
+  //   printf("%d", pids[i]);
+  // }
+  // printf("%d", procs_count);
+  return a;
 }
 
 // Esta función devuelve la función que se ejecutará en cada timer-interrupt
@@ -119,6 +239,7 @@ schedule_action_t get_scheduler(const char *name)
 {
   // Si necesitas inicializar alguna estructura antes de comenzar la simulación
   // puedes hacerlo aquí.
+
   if (strcmp(name, "fifo") == 0)
     return *fifo_scheduler;
   if (strcmp(name, "sjf") == 0)
@@ -127,6 +248,8 @@ schedule_action_t get_scheduler(const char *name)
     return *stcf_scheduler;
   if (strcmp(name, "rr") == 0)
     return *rr_scheduler;
+  if (strcmp(name, "mlfq") == 0)
+    return *mlfq_scheduler;
   fprintf(stderr, "Invalid scheduler name: '%s'\n", name);
   exit(1);
 }
